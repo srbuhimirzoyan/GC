8768635223aaf90d98a121711056a23aab5844b4
srbuhimirzoyan <38066772+srbuhimirzoyan@users.noreply.github.com>
2018-05-11 00:21:38 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,70 @@
 import json
 
-
-# ToDos
+#ToDos
 #   1.Limit the grades
 #   2.Order the list
 #   3.Grade should be a number
 #   4.
 
 def loadSetupData():
+
     with open('gc_setup.json') as data_file:
         course = json.load(data_file)
     setup = course["course_setup"]
     return setup
 
-
 def loadStudentGrades():
     with open("gc_grades.json") as data_file:
-        existing = json.load(data_file)
-    exist_gr = existing["mygrades"]
-    return exist_gr
+        existing=json.load(data_file)
+    return existing
 
 
-def askForAssignmentMarks(grades, st_grades):
-    current_grades = {"mygrades": {}}
+def askForAssignmentMarks(grades, gnahatakanner, student_id):
+    current_grades = {}
 
-    for key in grades:
-        try:
-            if st_grades[key] > -1:
-
-                "Your Grade from " + key + " is " + str(st_grades[key])
-                current_grades["mygrades"][key] = st_grades[key]
+    if gnahatakanner and (student_id in gnahatakanner):
+        for key in grades:
+            if gnahatakanner[student_id][key] > -1:
+                print "Your Grade from " + key + " is " + str(gnahatakanner[student_id][key])
+                current_grades[key] = gnahatakanner[student_id][key]
                 answer = raw_input("Do you want to update your score y/n")
                 if answer == "y":
-                    current_grades["mygrades"][key] = input("your new score")
+                    current_grades[key] = input("please enter your new score")
             else:
-                current_grades["mygrades"][key] = input(
-                    "What is your Current Grade for: " + key + " . Please insert -1 if you don't have a grade yet")
-        except:
-            current_grades["mygrades"][key] = input(
+                current_grades[key] = input("What is your Current Grade for: " + key + " . Please insert -1 if you don't have a grade yet")
+    else:
+        for key in grades:
+            current_grades[key] = input(
                 "What is your Current Grade for: " + key + " . Please insert -1 if you don't have a grade yet")
 
-    return current_grades
-
+    gnahatakanner[student_id] = current_grades
+    return gnahatakanner
 
 def saveGrades(current_grades):
-    print(json.dumps(current_grades))
+    print (json.dumps(current_grades))
     file = open("gc_grades.json", "w")
     file.write(json.dumps(current_grades))
     file.close()
 
-
-def printCurrentGrade(grades, current_grades):
+def printCurrentGrade(grades, current_grades,student_id):
     curr_grade = 0
-    for key in current_grades["mygrades"]:
-        if current_grades["mygrades"][key] != -1:
-            calc_grade = float(current_grades["mygrades"][key]) * grades[key] / 100
+    for key in current_grades[student_id]:
+        if current_grades[student_id][key] != -1:
+            calc_grade = float(current_grades[student_id][key]) * grades[key] / 100
             curr_grade = curr_grade + calc_grade
-    print(curr_grade)
+    print (curr_grade)
     return curr_grade
 
+def printLetterGrade(curr_grade,conv_matrix):
+    letter_grade = "N/A"
 
-def printLetterGrade(curr_grade, conv_matrix):
     for i in range(len(conv_matrix)):
-        if curr_grade > conv_matrix[i]["min"] and curr_grade <= conv_matrix[i]["max"]:
-            letter_grade = conv_matrix[i]["mark"]
-
+        if curr_grade>conv_matrix[i]["min"] and curr_grade<=conv_matrix[i]["max"]:
+            letter_grade=conv_matrix[i]["mark"]
+    print letter_grade
     return letter_grade
+
+
 
 
 
@@ -73,11 +72,10 @@ def main():
     setup = loadSetupData()
     grades = setup["grade_breakdown"]
     conv_matrix = setup["conv_matrix"]
-    st_grades = loadStudentGrades()
-    current_grades = askForAssignmentMarks(grades, st_grades)
+    student_id=raw_input("your id ??")
+    gnahatakanner = loadStudentGrades()
+    current_grades = askForAssignmentMarks(grades, gnahatakanner, student_id)
     saveGrades(current_grades)
-    curr_grade = printCurrentGrade(grades, current_grades)
-    letter = printLetterGrade(curr_grade, conv_matrix)
-
-
+    curr_grade = printCurrentGrade(grades, current_grades, student_id)
+    letter=printLetterGrade(curr_grade, conv_matrix)
 main()
